f138920950209cdb858304501e101d23bf58f070
Алексей Смирнов <debugger94@inbox.ru>
2017-10-15 11:06:46 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,6 @@
 ini_set("display_errors",1);
 error_reporting(E_ALL);
 
-define('ROOT_DIR', __DIR__);
-
 define('ENV', 'Cms');
-define('DS', DIRECTORY_SEPARATOR);
 
-require_once 'engine/Bootstrap.php';
+require_once 'engine/bootstrap.php';
