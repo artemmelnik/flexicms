b9afd76ebf84b720e6fb38d095722f3c438c329f
Алексей Смирнов <debugger94@inbox.ru>
2017-10-15 11:34:35 +0300

    Update defines.php

--- engine/defines.php
+++ engine/defines.php
@@ -1,7 +1,7 @@
 <?php
 
 // Some standard defines
-define('FLEXI_EXEC', true); // In the future in files defined('FLEXI') or die('Restricted access!')
+define('FLEXI_EXEC', true); // In the future in files defined('FLEXI_EXEC') or die('Restricted access!')
 define('FLEXI_VERSION', '0.0.1');
 define('DS', '/');
 
