112aa3955b7534b0b757918324b9a975b973f73a
Алексей Смирнов <debugger94@inbox.ru>
2017-10-15 11:05:17 +0300

    Create defines.php
    Moved definitions to defines.php and add some more.

---
+++ engine/defines.php
@@ -0,0 +1,11 @@
+<?php
+
+// Some standard defines
+define('FLEXI', true); // In the future in files defined('FLEXI') or die('Restricted access!')
+define('FLEXI_VERSION', '0.0.1');
+define('DS', '/');
+
+define('PHP_MIN', '5.4.0');
+
+// Directories and Paths
+define('ROOT_DIR', str_replace(DIRECTORY_SEPARATOR, DS, getcwd()));
