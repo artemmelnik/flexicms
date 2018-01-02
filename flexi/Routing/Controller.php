7d564baee5deae915d446bc70e2fa79a47ede5e8
Artem Melnik <artemmelnik989@gmail.com>
2018-01-02 14:56:16 +0200

    Adding the ability to switch layout in themes

--- flexi/Routing/Controller.php
+++ flexi/Routing/Controller.php
@@ -12,8 +12,33 @@ abstract class Controller
      */
     public $layout = 'main';
 
-    public function setLayout($layout)
+    /**
+     * @var array
+     */
+    public $data = [];
+
+    /**
+     * @param string $layout
+     */
+    public function setLayout(string $layout)
     {
         $this->layout = $layout;
+    }
+
+    /**
+     * @param string $key
+     * @param mixed $value
+     */
+    public function setData(string $key, $value)
+    {
+        $this->data[$key] = $value;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNameController()
+    {
+        return get_called_class();
     }
 }
